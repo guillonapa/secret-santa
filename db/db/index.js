2080508a21562d879a6c00bd72b54fafa88e41f1
guillonapa <guillonapa@gmail.com>
2019-11-24 12:03:28 -0500

    updated db connection credentials

--- db/db/index.js
+++ db/db/index.js
@@ -1,11 +1,13 @@
 const Pool = require('pg').Pool;
 const ENV = process.env;
 const pool = new Pool({
-    user: ENV.DB_USER,
-    host: ENV.DB_HOST,
-    database: ENV.DB_DB,
-    password: ENV.DB_PW,
-    port: ENV.DB_PORT
+    connectionString: ENV.DATABASE_URL,
+    ssl: true
+    // user: ENV.DB_USER,
+    // host: ENV.DB_HOST,
+    // database: ENV.DB_DB,
+    // password: ENV.DB_PW,
+    // port: ENV.DB_PORT
 });
 
 console.log("in db/index.js");
