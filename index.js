3fb1def2e956425939760a0be70047d4c3fb4cc5
guillonapa <guillonapa@gmail.com>
2019-11-24 18:13:07 -0500

    ...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mountRoutes = require('./routes/index');
+const db = require('./db/index');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
@@ -24,6 +25,31 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/api', (request, response) => {
   response.send("Express API is up and running!");
 });
+
+app.get('/:eventKey/:personalKey', (req, res) => {
+    const { eventKey, personalKey } = req.params;
+
+    console.log("eventKey: ", eventKey);
+    console.log("personalKey: ", personalKey);
+
+    const result = await db.query(`SELECT name FROM people WHERE personal_key IN (SELECT match FROM people WHERE personal_key = '${personalKey}' AND event_key = '${eventKey}')`);
+    if (!result) {
+        console.log("Error querying");
+    }
+    console.log("Ultimate result: ", result);
+    console.log("Done!!!");
+
+    const { rows } = result;
+    console.log("Rows: " + rows);
+    if (!rows || rows.length == 0) {
+        res.send("Something is not right... Check your keys and try again");
+        return;
+    }
+
+    const name = rows[0].name;
+    console.log("Name: " + name);
+    res.send(name ? name : "Something is not right... Check your keys and try again");
+})
 
 // Anything that doesn't match the above, send back the index.html file
 app.get('*', (req, res) => {
