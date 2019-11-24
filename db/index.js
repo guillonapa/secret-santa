a515d2827adf1e3eac37b124dc8a73c9b40546b0
guillonapa <guillonapa@gmail.com>
2019-11-24 16:26:00 -0500

    ...

--- db/index.js
+++ db/index.js
@@ -1,38 +1,18 @@
-const express = require('express');
-const mountRoutes = require('./routes/index');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
-
+const Pool = require('pg').Pool;
 const ENV = process.env;
-const port = ENV.PORT || ENV.REACT_APP_API_PORT; //3001;
-
-console.log("First: ", ENV.REACT_APP_API_PORT);
-console.log("Second: ", ENV.MAILER_EMAIL);
-
-app.use(cors());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('/api', (request, response) => {
-  response.send("Express API is up and running!");
+const pool = new Pool({
+    connectionString: ENV.DATABASE_URL,
+    ssl: true
+    // user: ENV.DB_USER,
+    // host: ENV.DB_HOST,
+    // database: ENV.DB_DB,
+    // password: ENV.DB_PW,
+    // port: ENV.DB_PORT
 });
 
-// Anything that doesn't match the above, send back the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/build/index.html'));
-})
+console.log("in db/index.js");
 
-// app.post('/event', db.createEvent);
 
-mountRoutes(app);
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-});
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+};
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mountRoutes = require('./routes/index');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const ENV = process.env;
+const port = ENV.PORT || ENV.REACT_APP_API_PORT; //3001;
+
+console.log("First: ", ENV.REACT_APP_API_PORT);
+console.log("Second: ", ENV.MAILER_EMAIL);
+
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api', (request, response) => {
+  response.send("Express API is up and running!");
+});
+
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client/build/index.html'));
+})
+
+// app.post('/event', db.createEvent);
+
+mountRoutes(app);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+});
